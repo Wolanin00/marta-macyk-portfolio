1daf7833048d2b2a1220f3bd09a8ada67c584d60
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-08-09 23:01:36 +0200

    test

--- api/server.py
+++ api/server.py
@@ -1,6 +1,9 @@
+import os.path
+
 from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -10,6 +13,11 @@ def index():
 @app.route("/project-1")
 def open_projektowanie_wstepne_architektoniczno_urbanistyczne_ii():
     return send_file(path_or_file='./static/files/Projektowanie-wstepne-architektoniczno-urbanistyczne-II-compressed.pdf', as_attachment=False)
+
+
+@app.route('/test')
+def test():
+    return send_file(path_or_file=os.path.join(os.path.dirname(__file__), "static/files/test.pdf"), as_attachment=False)
 
 
 if __name__ == "__main__":
