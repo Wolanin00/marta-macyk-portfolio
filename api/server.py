e729c7261b0dada423ba6e054e8cf5ba094aa36b
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-08-28 21:21:06 +0200

    add download link

--- api/server.py
+++ api/server.py
@@ -1,4 +1,5 @@
 import os.path, json
+import wget
 
 from flask import Flask, render_template, send_file, send_from_directory
 
@@ -12,16 +13,9 @@ def index():
 
 @app.route("/project-1")
 def open_projektowanie_wstepne_architektoniczno_urbanistyczne_ii():
-    return send_file(path_or_file='./static/files/Projektowanie-wstepne-architektoniczno-urbanistyczne-II-compressed.pdf', as_attachment=False)
-
-
-@app.route('/test')
-def test():
-    workingdir = os.path.abspath(os.getcwd())
-    # test
-    filepath = workingdir + '/static/files/'
-    return send_from_directory(filepath, 'test.pdf', as_attachment=False)
-    # return send_file(path_or_file="./static/files/test.pdf", as_attachment=False)
+    url = 'https://github.com/Wolanin00/marta-macyk-portfolio/blob/main/api/static/files/Projektowanie-wstepne-architektoniczno-urbanistyczne-II-compressed.pdf'
+    filename = wget.download(url)
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
