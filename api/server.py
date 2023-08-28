1a4415e0118e333bf9524f06393aca3bec07cd26
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-08-28 21:50:11 +0200

    update project_1

--- api/server.py
+++ api/server.py
@@ -10,19 +10,9 @@ def index():
     return render_template('index.html')
 
 
-@app.route("/project-1")
-def open_projektowanie_wstepne_architektoniczno_urbanistyczne_ii():
-    url = 'https://github.com/Wolanin00/marta-macyk-portfolio/blob/main/api/static/files/Projektowanie-wstepne-architektoniczno-urbanistyczne-II-compressed.pdf'
-    return render_template('index.html')
-
-
-@app.route('/test')
-def test():
-    workingdir = os.path.abspath(os.getcwd())
-    # test
-    filepath = workingdir + '/static/files/'
-    return send_from_directory(filepath, 'test.pdf', as_attachment=False)
-    # return send_file(path_or_file="./static/files/test.pdf", as_attachment=False)
+@app.route("/project_1")
+def open_project_1():
+    return render_template('project_1.html')
 
 
 if __name__ == "__main__":
