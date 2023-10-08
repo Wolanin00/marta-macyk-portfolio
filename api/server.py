46e54e1f307aec179ece7f011a0c534072587b47
Mateusz Szewczyk 1 <szewczykmateusz64@gmail.com>
2023-10-08 20:22:30 +0200

    add project 2

--- api/server.py
+++ api/server.py
@@ -13,5 +13,10 @@ def open_project_1():
     return render_template('project_1.html')
 
 
+@app.route("/project_2")
+def open_project_2():
+    return render_template('project_2.html')
+
+
 if __name__ == "__main__":
     app.run(debug=True)
